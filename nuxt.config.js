e073b50385026de712feac371c2ef683867c75be
Ilán Vivanco <ilanvivanco@gmail.com>
2020-08-06 19:49:35 -0300

    Change Nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,77 +1,81 @@
-
 export default {
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'universal',
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'static',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Global CSS
-  */
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-    '@/plugins/element-ui'
-  ],
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
-  components: true,
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    // Doc: https://github.com/nuxt/content
-    '@nuxt/content',
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {},
-  /*
-  ** Content module configuration
-  ** See https://content.nuxtjs.org/configuration
-  */
-  content: {},
-  /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
-    transpile: [/^element-ui/],
-  }
+	/*
+	** Nuxt rendering mode
+	** See https://nuxtjs.org/api/configuration-mode
+	*/
+	mode: 'universal',
+	/*
+	** Nuxt target
+	** See https://nuxtjs.org/api/configuration-target
+	*/
+	target: 'static',
+	/*
+	** Headers of the page
+	** See https://nuxtjs.org/api/configuration-head
+	*/
+	head: {
+		titleTemplate: '%s - Ilán Vivanco',
+		title: 'Full Stack Developer',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
+	/*
+	** Global CSS
+	*/
+	css: [
+		'element-ui/lib/theme-chalk/index.css',
+		'@/assets/css/theme.css',
+		'@/assets/css/main.scss',
+	],
+	/*
+	** Plugins to load before mounting the App
+	** https://nuxtjs.org/guide/plugins
+	*/
+	plugins: [
+		'@/plugins/element-ui'
+	],
+	/*
+	** Auto import components
+	** See https://nuxtjs.org/api/configuration-components
+	*/
+	components: true,
+	/*
+	** Nuxt.js dev-modules
+	*/
+	buildModules: [
+		// Doc https://github.com/nuxt-community/dotenv-module
+		'@nuxtjs/dotenv',
+	],
+	/*
+	** Nuxt.js modules
+	*/
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
+		// Doc: https://github.com/nuxt/content
+		'@nuxt/content',
+	],
+	/*
+	** Axios module configuration
+	** See https://axios.nuxtjs.org/options
+	*/
+	axios: {},
+	/*
+	** Content module configuration
+	** See https://content.nuxtjs.org/configuration
+	*/
+	content: {},
+	/*
+	** Build configuration
+	** See https://nuxtjs.org/api/configuration-build/
+	*/
+	build: {
+		transpile: [/^element-ui/],
+	}
 }
