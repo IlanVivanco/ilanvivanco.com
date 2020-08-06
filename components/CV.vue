cfda597c2c7818a65e8ec1bf93b966c493684abf
Ilán Vivanco <ilanvivanco@gmail.com>
2020-08-06 19:50:09 -0300

    Start CV Component

---
+++ components/CV.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="cv">
+		<br />
+		<pre>
+			{{ cv_data }}
+		</pre
+		>
+	</div>
+</template>
+
+<script>
+import CV from '@/content/cv.js'
+
+export default {
+	props: {
+		lang: {
+			type: String,
+			default: 'es',
+		},
+	},
+	computed: {
+		cv_data() {
+			return CV[this.lang]
+		},
+	},
+}
+</script>
+
+<style>
+</style>
