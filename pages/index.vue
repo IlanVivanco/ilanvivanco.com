6d115ae6e02cbb0002727619e0c12750143cc076
Ilán Vivanco <ilanvivanco@gmail.com>
2020-08-06 19:53:25 -0300

    First changes to homepage [WIP]

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,65 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        ilanvivanco.com
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+	<div class="page">
+		<el-container>
+			<el-row>
+				<el-col :span="2">2</el-col>
+				<el-col :span="10">10</el-col>
+				<el-col :span="12">12</el-col>
+			</el-row>
+		</el-container>
+
+		<div class="container">
+			<div>
+				<h1 class="title">
+					ilanvivanco.com
+				</h1>
+				<div class="links">
+					<el-button v-for="post in posts" :key="post.slug" type="primary">
+						<a :href="`/blog/${post.slug}`">{{ post.title }}</a>
+					</el-button>
+					<el-button
+						type="primary"
+						icon="el-icon-edit"
+						plain
+						circle
+					></el-button>
+				</div>
+
+				<Cv lang="es" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {}
+import Cv from '@/components/CV'
+
+export default {
+	head() {
+		return {
+			title: 'My Index title',
+		}
+	},
+	async asyncData({ $content }) {
+		const posts = await $content('posts').fetch()
+
+		return {
+			posts,
+		}
+	},
+	components: {
+		Cv,
+	},
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss">
+@import '@/assets/css/variables';
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.page {
+	max-width: 1200px;
+	margin: 0 auto;
+	height: 90vh;
+	background: $color-grayscale-1;
 }
 </style>
