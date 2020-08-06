c8a1300f5810bc8b83db51668a05fa509448dad0
Ilán Vivanco <ilanvivanco@gmail.com>
2020-08-06 19:49:56 -0300

    Add CV data file

---
+++ content/cv.js
@@ -0,0 +1,18 @@
+export default {
+	es: {
+		name: "Ilán Vivanco",
+		job_title: "Programador web full stack",
+		email: "ilanvivanco@gmail.com",
+		phone: "(+54) 11.5982.6302",
+		birth_date: "17/06/1988",
+		location: "Puerto Madryn, Argentina"
+	},
+	en: {
+		name: "Ilán Vivanco",
+		job_title: "Full Stack Web Developer",
+		email: "ilanvivanco@gmail.com",
+		phone: "(+54) 11.5982.6302",
+		birth_date: "17/06/1988",
+		location: "Puerto Madryn, Argentina"
+	}
+}
