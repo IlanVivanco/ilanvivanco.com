faa55ef39bd14744d88246bae4e84b7eadf21fd3
Ilán Vivanco <ilanvivanco@gmail.com>
2020-08-13 19:42:29 -0300

    Nav tooltips delay

--- components/global/nav.vue
+++ components/global/nav.vue
@@ -1,31 +1,35 @@
 <template>
 	<div>
-		<el-menu
-			:default-active="$route.path"
-			mode="vertical"
-			:collapse="true"
-			:router="true"
-		>
-			<el-menu-item :index="localePath('index')">
-				<i class="el-icon-user"></i>
-				<span slot="title">Sobre mi</span>
-			</el-menu-item>
-			<el-menu-item :index="localePath('resume')">
-				<i class="el-icon-tickets"></i>
-				<span slot="title">Curriculum</span>
-			</el-menu-item>
-			<el-menu-item :index="localePath('portfolio')">
-				<i class="el-icon-monitor"></i>
-				<span slot="title">Trabajos</span>
-			</el-menu-item>
-			<el-menu-item :index="localePath('blog')">
-				<i class="el-icon-notebook-1"></i>
-				<span slot="title">Blog</span>
-			</el-menu-item>
-			<el-menu-item :index="localePath('contact-me')">
-				<i class="el-icon-chat-round"></i>
-				<span slot="title">Contactame</span>
-			</el-menu-item>
+		<el-menu :default-active="$route.path" mode="vertical" :collapse="true" :router="true">
+			<el-tooltip effect="dark" placement="right" content="Sobre mi" :open-delay="400">
+				<el-menu-item :index="localePath('index')">
+					<i class="el-icon-user"></i>
+				</el-menu-item>
+			</el-tooltip>
+
+			<el-tooltip effect="dark" placement="right" content="Curriculum" :open-delay="400">
+				<el-menu-item :index="localePath('resume')">
+					<i class="el-icon-tickets"></i>
+				</el-menu-item>
+			</el-tooltip>
+
+			<el-tooltip effect="dark" placement="right" content="Trabajos" :open-delay="400">
+				<el-menu-item :index="localePath('portfolio')">
+					<i class="el-icon-monitor"></i>
+				</el-menu-item>
+			</el-tooltip>
+
+			<el-tooltip effect="dark" placement="right" content="Blog" :open-delay="400">
+				<el-menu-item :index="localePath('blog')">
+					<i class="el-icon-notebook-1"></i>
+				</el-menu-item>
+			</el-tooltip>
+
+			<el-tooltip effect="dark" placement="right" content="Contactame" :open-delay="400">
+				<el-menu-item :index="localePath('contact-me')">
+					<i class="el-icon-chat-round"></i>
+				</el-menu-item>
+			</el-tooltip>
 		</el-menu>
 	</div>
 </template>
