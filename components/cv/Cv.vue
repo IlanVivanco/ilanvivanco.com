15f7e6ca52b4186690c82811c1fe74c27334944a
Ilán Vivanco <ilanvivanco@gmail.com>
2020-10-08 16:57:41 -0300

    CV component

---
+++ components/cv/Cv.vue
@@ -0,0 +1,32 @@
+<template>
+	<section>
+		<el-row align="middle" :gutter="20">
+			<el-col :span="12">
+				<positions :data="cvData.positions" />
+			</el-col>
+			<el-col :span="12">
+				<positions :data="cvData.positions" />
+			</el-col>
+		</el-row>
+	</section>
+</template>
+
+<script>
+import Positions from '@/components/cv/Positions'
+
+export default {
+	name: 'CV',
+	components: { Positions },
+	data() {
+		return {
+			cvData: { positions: [] },
+		}
+	},
+	mounted() {
+		this.cvData = this.$t('cv.data')
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
