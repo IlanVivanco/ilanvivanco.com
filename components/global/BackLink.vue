07cf3282e2aa009702a14b6753ed3f1e00f2f597
Ilán Vivanco <ilanvivanco@gmail.com>
2020-10-22 08:58:44 -0300

    Create BackLink component

---
+++ components/global/BackLink.vue
@@ -0,0 +1,20 @@
+<template>
+	<nuxt-link :to="to" class="back-link"><i class="el-icon-back"></i> Volver</nuxt-link>
+</template>
+
+<script>
+export default {
+	name: 'BackLink',
+	props: {
+		to: String,
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.back-link {
+	text-decoration: none;
+	color: $color-grayscale-7;
+	font-weight: 400;
+}
+</style>
