a627d1fc15418422175907f5a200df2b5a7c1336
Ilán Vivanco <ilanvivanco@gmail.com>
2020-10-23 02:52:34 -0300

    Fix status code on error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,19 +1,16 @@
 <template>
 	<div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
-  </div>
+		<h1 v-if="error.statusCode === 404">Page not found</h1>
+		<h1 v-else>An error occurred</h1>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, veritatis, quam sint deserunt vel dolorum quod quae blanditiis eveniet id amet aperiam qui inventore necessitatibus aspernatur ad fugiat esse nam.
+		<nuxt-link to="/">Home page</nuxt-link>
+	</div>
 </template>
 
 <script>
 export default {
 	name: 'ErrorPage',
-	components: {},
-	transition: { mode: '' },
-	data() {
-		return {}
-	},
+	props: ['error'],
 }
 </script>
 
