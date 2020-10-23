bd9fb1dd5c2fa2aa934676667ecfceae79d88956
Ilán Vivanco <ilanvivanco@gmail.com>
2020-10-23 00:12:39 -0300

    Create ErrorPage base

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="container">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'ErrorPage',
+	components: {},
+	transition: { mode: '' },
+	data() {
+		return {}
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
