a6d81d481953c849e82a93fd00f8c297073d81ef
Ilán Vivanco <ilanvivanco@gmail.com>
2020-10-24 23:31:01 -0300

    Changes on index [WIP]

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,39 @@
 <template>
-	<div class="container" id="sobre-mi">
-		<div>
-			<section-title title="Hola Mundo" description="" />
+	<div class="container about">
+		<el-row align="middle" :gutter="30">
+			<el-col :span="24" class="about__intro">
+				<h3>¡Hola! Mi nombre es</h3>
+				<h1>Ilán Vivanco.</h1>
+				<h2>Desarrollador web full stack.</h2>
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iste inventore consectetur aut laudantium
-				mollitia sint aliquid nulla asperiores assumenda recusandae similique, non perferendis fugit reprehenderit
-				cum debitis? Fuga, adipisci!
-			</p>
+				<p>
+					Soy desarrollador web full stack experto en WordPress trabajando en remoto como freelancer para empresas
+					internacionales desde Argentina. Especializado en el armado —y muy ocasionalmente diseñando— de sitios
+					web hechos a medida en WordPress.
+				</p>
+			</el-col>
+		</el-row>
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iste inventore consectetur aut laudantium
-				mollitia sint aliquid nulla asperiores assumenda recusandae similique, non perferendis fugit reprehenderit
-				cum debitis? Fuga, adipisci!
-			</p>
+		<el-row align="middle" :gutter="30" type="flex">
+			<el-col :span="12" class="about__data">
+				<section-title title="sobre mi" type="h2" />
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iste inventore consectetur aut laudantium
-				mollitia sint aliquid nulla asperiores assumenda recusandae similique, non perferendis fugit reprehenderit
-				cum debitis? Fuga, adipisci!
-			</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, nihil qui sunt at dolorem officia
+					sed vitae. Hic ad aspernatur voluptas magni itaque exercitationem sint doloremque consequatur corporis!
+					Maiores, dignissimos.
+				</p>
+			</el-col>
+			<el-col :span="12" class="about__data">
+				<section-title title="mucho más" type="h2" />
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iste inventore consectetur aut laudantium
-				mollitia sint aliquid nulla asperiores assumenda recusandae similique, non perferendis fugit reprehenderit
-				cum debitis? Fuga, adipisci!
-			</p>
-		</div>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, nihil qui sunt at dolorem officia
+					sed vitae. Hic ad aspernatur voluptas magni itaque exercitationem sint doloremque consequatur corporis!
+					Maiores, dignissimos.
+				</p>
+			</el-col>
+		</el-row>
 	</div>
 </template>
 
@@ -48,4 +55,29 @@ export default {
 </script>
 
 <style lang="scss">
+.about__intro {
+	margin-bottom: 2rem;
+
+	h1 {
+		line-height: 1.2;
+	}
+
+	h2 {
+		color: $color-blue;
+		line-height: 1.2;
+		font-size: 1.3em;
+	}
+
+	h3 {
+		color: $color-teal-dark;
+		font-weight: normal;
+		font-size: 0.9rem;
+	}
+
+	p {
+		margin-top: 1rem;
+		font-style: italic;
+		line-height: 1.4;
+	}
+}
 </style>
