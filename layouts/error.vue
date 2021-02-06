83461d9ddd39b66eddfff3e8ee28933dab1541aa
Ilán Vivanco <ilanvivanco@gmail.com>
2021-02-06 16:46:48 +0100

    Add error page.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,14 @@
 <template>
-	<div class="container">
-		<h1 v-if="error.statusCode === 404">Page not found</h1>
-		<h1 v-else>An error occurred</h1>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, veritatis, quam sint deserunt vel dolorum quod quae blanditiis eveniet id amet aperiam qui inventore necessitatibus aspernatur ad fugiat esse nam.
-		<nuxt-link to="/">Home page</nuxt-link>
+	<div class="container error-404 main-content">
+		<div class="error-404__message">
+			<div class="robot-wrapper">
+				<h2>{{ error.statusCode }}</h2>
+				<img src="/images/404.png" alt="Not found" />
+			</div>
+
+			<h1 v-if="error.statusCode === 404">{{ $t('general.not_found') }}</h1>
+			<h1 v-else>{{ $t('general.error') }}</h1>
+		</div>
 	</div>
 </template>
 
@@ -15,4 +20,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.robot-wrapper {
+	position: relative;
+
+	h2 {
+		font-size: 3rem;
+		position: absolute;
+		font-weight: bold;
+		left: 50%;
+		top: 0;
+		transform: translateX(-50%);
+		color: $color-blue-dark;
+
+		@supports (-webkit-text-stroke: 2px $color-blue-dark) {
+			color: $color-white;
+			-webkit-text-stroke: 2px $color-blue-dark;
+		}
+	}
+}
+
+.error-404 {
+	display: grid;
+	place-items: center;
+
+	&__message {
+		width: 50%;
+
+		h1 {
+			text-align: center;
+			font-weight: normal;
+		}
+
+		img {
+			max-width: 100%;
+			opacity: 0.9;
+		}
+	}
+}
 </style>
