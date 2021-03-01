dfe84f09dbc22ed7ed1d31a50469898ccdd6b1b3
Ilán Vivanco <ilanvivanco@gmail.com>
2021-03-01 14:15:05 +0100

    Refactor instagram-fetch to read data from prod site before fetching.

--- instagram-fetch.js
+++ instagram-fetch.js
@@ -2,33 +2,64 @@ const ENV = require('./env.js');
 const axios = require('axios');
 const fs = require('fs')
 
-const options = {
-	method: 'GET',
-	url: ENV.instagram.url,
-	params: { username: ENV.instagram.user },
-	headers: {
-		'x-rapidapi-key': ENV.instagram.key,
-		'x-rapidapi-host': ENV.instagram.host
+// Fetch last run from prod site
+async function fetchLastRun() {
+	try {
+		const instagramJSON = await axios.request({
+			method: 'GET',
+			url: `${ENV.base_url}/data/instagram.json`,
+		})
+
+		return instagramJSON.data.last_run;
+	} catch (error) {
+		console.error(error);
 	}
-};
 
-const DAY_IN_SECONDS = 60 * 60 * 24 * 1000;
-const instagramJSON = fs.readFileSync(ENV.instagram.dest, { encoding: 'utf8', flag: 'r' });
-const currentData = JSON.parse(instagramJSON);
-const nextRun = (currentData.last_run) ? currentData.last_run + DAY_IN_SECONDS : 0;
-const now = new Date().getTime();
+	// If nothing's found let's fetch it
+	return 0
+}
 
-// Only fetch new data once a day maximum
-if (now < nextRun) return;
+// Should we fetch new data or not?
+async function shouldFetchData() {
+	const DAY_IN_SECONDS = 60 * 60 * 24 * 1000;
+	const lastRun = await fetchLastRun()
+	const nextRun = lastRun + DAY_IN_SECONDS;
+	const now = new Date().getTime();
 
-// Fetch Instagram data
-axios.request(options).then((response) => {
+	return nextRun < now;
+}
+
+// Maybe fetch new data
+async function maybeFetchInstagramData() {
+	const shouldFetch = await shouldFetchData();
+
+	// Fetch only once a day
+	if (!shouldFetch) return;
+
+	try {
+		const instaData = await axios.request({
+			method: 'GET',
+			url: ENV.instagram.url,
+			params: { username: ENV.instagram.user },
+			headers: {
+				'x-rapidapi-key': ENV.instagram.key,
+				'x-rapidapi-host': ENV.instagram.host
+			}
+		})
+
+		saveTheData(instaData.data)
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function saveTheData(data) {
 	const instagram = {
 		last_run: new Date().getTime(),
-		data: response.data,
+		data: data,
 	};
 
 	fs.writeFileSync(ENV.instagram.dest, JSON.stringify(instagram))
-}).catch((error) => {
-	console.error(error);
-});
+}
+
+maybeFetchInstagramData()
