ad9f7f4fbb62d3f307ad412ec87c55e024f18193
Ilán Vivanco <ilanvivanco@gmail.com>
2021-04-10 19:52:20 +0200

    Update Instagram JSON data

--- instagram-fetch.js
+++ instagram-fetch.js
@@ -1,5 +1,5 @@
-const ENV = require('./env.js');
-const axios = require('axios');
+const ENV = require('./env.js')
+const axios = require('axios')
 const fs = require('fs')
 
 // Fetch last run from prod site
@@ -10,9 +10,9 @@ async function fetchLastRun() {
 			url: `${ENV.base_url}/data/instagram.json`,
 		})
 
-		return instagramJSON.data.last_run;
+		return instagramJSON.data.last_run
 	} catch (error) {
-		console.error(error);
+		console.error(error)
 	}
 
 	// If nothing's found let's fetch it
@@ -21,25 +21,27 @@ async function fetchLastRun() {
 
 // Should we fetch new data or not?
 async function shouldFetchData() {
-	const DAY_IN_SECONDS = 60 * 60 * 24 * 1000;
+	const DAY_IN_SECONDS = 60 * 60 * 24 * 1000
 	const lastRun = await fetchLastRun()
-	const nextRun = lastRun + DAY_IN_SECONDS;
-	const now = new Date().getTime();
+	const nextRun = lastRun + DAY_IN_SECONDS
+	const now = new Date().getTime()
 
-	return nextRun < now;
+	console.log(`Last ran was on: ${new Date(lastRun)}`)
+
+	return nextRun < now
 }
 
 // Maybe fetch new data
 async function maybeFetchInstagramData() {
-	const shouldFetch = await shouldFetchData();
+	const shouldFetch = await shouldFetchData()
 
 	// Fetch only once a day
 	if (!shouldFetch) {
-		console.log('Instagram data is up-to-date.');
-		return;
+		console.log('Instagram data is up-to-date.')
+		return
 	}
 
-	console.log('Fetching new instagram data...');
+	console.log('Fetching new instagram data...')
 
 	try {
 		const instaData = await axios.request({
@@ -48,13 +50,13 @@ async function maybeFetchInstagramData() {
 			params: { username: ENV.instagram.user },
 			headers: {
 				'x-rapidapi-key': ENV.instagram.key,
-				'x-rapidapi-host': ENV.instagram.host
-			}
+				'x-rapidapi-host': ENV.instagram.host,
+			},
 		})
 
 		saveTheData(instaData.data)
 	} catch (error) {
-		console.error(error);
+		console.error(error)
 	}
 }
 
@@ -62,7 +64,7 @@ function saveTheData(data) {
 	const instagram = {
 		last_run: new Date().getTime(),
 		data: data,
-	};
+	}
 
 	fs.writeFileSync(ENV.instagram.dest, JSON.stringify(instagram))
 }
