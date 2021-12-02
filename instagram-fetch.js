06cdba52955d8f0cf0df0da613dfcff77c857d6c
Ilán Vivanco <ilanvivanco@gmail.com>
2021-12-02 17:05:01 +0100

    Update instagram task script

--- instagram-fetch.js
+++ instagram-fetch.js
@@ -2,69 +2,34 @@ const ENV = require('./env.js')
 const axios = require('axios')
 const fs = require('fs')
 
-// Fetching params
-const [force] = process.argv.slice(2)
-
-const DAY_IN_MILISECONDS = 60 * 60 * 24 * 1000
-const now = new Date()
-let lastRun = null
-let nextRun = null
-
-// Fetch the last run unix time from prod site
-async function fetchLastRun() {
+// Fetch only once a day or if force is passed
+async function fetchInstagramData() {
 	try {
-		const instagramJSON = await axios.request({
+		const instaData = await axios({
 			method: 'GET',
-			url: `${ENV.base_url}/data/instagram.json`,
+			url: ENV.instagram.url,
+			params: {
+				limit: 12,
+				date: Date.now(),
+			},
 		})
 
-		return instagramJSON.data.last_run
+		saveTheData(instaData.data)
 	} catch (error) {
 		console.error(error)
-	}
-
-	return 0
-}
-
-// Fetch only once a day or if force is passed
-async function maybeFetchInstagramData(force = false) {
-	lastRun = await fetchLastRun()
-	nextRun = lastRun + DAY_IN_MILISECONDS
-
-	const shouldFetch = force || now.getTime() > nextRun
-
-	// Should we fetch new data or not?
-	if (shouldFetch) {
-		console.info(`Fetching new data. Latest was from: ${new Date(lastRun)}`)
-
-		try {
-			const instaData = await axios({
-				method: 'GET',
-				url: ENV.instagram.url,
-				params: {
-					limit: 12,
-					date: Date.now(),
-				},
-			})
-
-			saveTheData(instaData.data)
-		} catch (error) {
-			console.error(error)
-		}
-	} else {
-		console.info(`Not fetching data. Next update will be on: ${new Date(nextRun)}`)
 	}
 }
 
 // Save the data to the file
-function saveTheData(data) {
+function saveTheData({data}) {
+	const now = new Date()
 	const instagram = {
-		last_run: now.getTime(),
-		last_run_on: now.toLocaleString(),
-		data: data.data,
+		updated_at: now.toLocaleString(),
+		data,
 	}
 
 	fs.writeFileSync(ENV.instagram.dest, JSON.stringify(instagram))
+	console.info(`Instagram data saved on ${now.toLocaleString()}.`)
 }
 
-maybeFetchInstagramData(force)
+fetchInstagramData()
