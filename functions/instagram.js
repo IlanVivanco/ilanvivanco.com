17cbc9752368f373927c24071318c32f60225d54
Ilán Vivanco <ilanvivanco@gmail.com>
2022-02-15 12:34:25 +0100

    Fix instagram function

--- functions/instagram.js
+++ functions/instagram.js
@@ -25,7 +25,7 @@ exports.handler = async function(event, context) {
 // Fetch new data only once a day or if force is passed.
 async function getInstagramData(force = false) {
 	const dayInMillliseconds = 60 * 60 * 24 * 1000
-	const lastRun = await fetchLastRun() | 0
+	const lastRun = await fetchLastRun()
 	const nextRun = lastRun + dayInMillliseconds
 	const shouldFetch = force || now.getTime() > nextRun
 	let instaData = undefined
@@ -66,7 +66,7 @@ async function readFile() {
 	try {
 		return await fs.readFile(instagramDataFile).then(JSON.parse)
 	} catch (error) {
-		console.error(error)
+		console.info(error)
 	}
 }
 
@@ -88,7 +88,8 @@ async function fetchData() {
 async function fetchLastRun() {
 	try {
 		const instagramJSON = await readFile()
-		return instagramJSON?.updated_unix
+
+		return instagramJSON?.updated_unix || 0
 	} catch (error) {
 		console.error(error)
 	}
